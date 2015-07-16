9311c5c63973aebca6d3bb5c25b9a70128ee733e
liulei020 <158675345@qq.com>
2015-07-16 15:04:13 +0800

    add assignment_4.1 Updata about how to calculate AverageScore & Fix two bug(NaN)

--- AverageScore.java
+++ AverageScore.java
@@ -5,97 +5,51 @@ import java.util.Scanner;
 
 public class AverageScore {
 
+	private static final Exception Exception = null;
 	private static Integer grades;
-	private static boolean judgement;
-
+	private static String str;
+	static ArrayList<Integer> scores;
+	static String[] temp;
 
 	static double averageScore(ArrayList<Integer> scores){
 		double sum=0;
-		int n=scores.size()-1;
-		for(int i=0;i<n;i++){
+		for(int i=0;i<scores.size();i++){
 			sum+=scores.get(i);
 		}
-		return sum/n;
+		return sum/scores.size();
 	}
 
-	/**
-	{
-	double sum=0,tmp = 0;
-	int n=scores.size()-1;
-	for(Integer x:scores){
-		tmp+=x;
-		sum=tmp+1;
+	public static void inputData(){
+
+		Scanner scanner = new Scanner(System.in);
+		str = scanner.next();
+		temp = str.split(",");
 	}
-	return sum/n;
-	}
-	 */
-
-	/**
-	{
-	int i=0;
-	double sum=0;
-	int n=scores.size()-1;
-	while(i<n){
-		sum+=scores.get(i);
-		i++;
-	}
-	return sum/n;
-    } 
-	 */
-
-	/**
-	int i=0;
-	double sum=0;
-	int n=scores.size()-1;
-	do{
-		sum+=scores.get(i);
-		i++;
-	}while(i<n);
-	return sum/n;
-    }
-	 */
-	public static boolean isNumeric(String str){  
-		int n=str.length()-2;
-		for (int i=0;i<n;i++){    
-			if (!Character.isDigit(str.charAt(i))){  
-				return false;  
-			}  
-		}  
-		return true;  
-	}  
-
-
-	public static void main(String[] args) {   
-		System.out.println("如果你想结束输入，请输入'-1'作为一条结束命令，从而求取上列成绩的平均分:");
-		System.out.println();
-		System.out.println("请输入你的各科成绩:");
-		ArrayList<Integer> scores = new ArrayList<Integer>();
-		do{
-			Scanner scanner = new Scanner(System.in);    
-			String str = scanner.next();
-			judgement =isNumeric(str);
-			grades =Integer.parseInt(str);
-			scores.add(grades);
-		}while(grades!=-1);
-		if(judgement==true){
-			System.out.println(averageScore(scores));
-		}else{
-			System.out.println("输入有误！！");
-			System.out.println("请重新输入成绩(纯数字类型):");
-			/**
-			do{
-				Scanner scanner = new Scanner(System.in);    
-				String str = scanner.next();
-				judgement =isNumeric(str);
-				grades =Integer.parseInt(str);
+	public static void judge(){
+		try{
+			int k=0;
+			grades =Integer.parseInt(temp[k]);
+			if(grades<0){
+				throw Exception;
+			} 
+		}catch(Exception e){
+			System.out.println("输入有误：");
+			inputData();
+			judge();
+		}finally{
+			for(int k= 0;k < temp.length;k++){
 				scores.add(grades);
-			}while(grades!=-1);
-			System.out.println(averageScore(scores));
-			 */
-			//Question_1:isNumeric()判断非数字返回false，继而我又用if条件报错，为什么输入字母/特殊符号还是不能检测？
-
-			//Question_1:底部备注部分，重新进行数据输入，但是加上这段，系统报错NaN!!
+			}
 		}
 	}
+	public static void main(String[] args) {   
+		System.out.println();
+		System.out.println("请输入若干个学生成绩(以逗号分隔)：");
+		scores = new ArrayList<Integer>();
+		inputData();
+		judge();
+		System.out.println(averageScore(scores));
+	}
 }
+
 
