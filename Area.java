21326d30c0188ea08f5603ed06699612f07ff2a7
liulei020 <158675345@qq.com>
2015-07-16 10:00:52 +0800

    add assignment_3 Updata Calculate the area of a circle

--- Area.java
+++ Area.java
@@ -4,15 +4,15 @@ import java.lang.Math;
 import java.util.Scanner;
 
 public class Area {
-	 public Area(double  radius){
-		 double  CircleArea; 
-		 CircleArea= Math.PI* radius* radius;
-		 System.out.println("半径为"+ radius +"的圆面积为："+CircleArea);
-		}
+	public Area(double  radius){
+		double  CircleArea; 
+		CircleArea= Math.PI* radius* radius;
+		System.out.println("半径为"+ radius +"的圆面积为："+CircleArea);
+	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);    
-	    System.out.println("请输入该圆半径:");  
-	    int  radius = scanner.nextInt();
+		System.out.println("请输入该圆半径:");  
+		int  radius = scanner.nextInt();
 		new Area(radius);
 
 	}
