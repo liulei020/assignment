b5b07e4508c8618d55d8f05ad6d075ecde22e110
liulei020 <158675345@qq.com>
2015-07-21 09:52:13 +0800

    add assignment_5.1 about ZipCode

---
+++ ZipCode.java
@@ -0,0 +1,43 @@
+package Regex;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ZipCode {
+
+	public static void main(String[] args) throws IOException {
+		FileReader reader = new FileReader("C:\\Users\\H\\Desktop\\12014020\\assignment_4begin" +
+				"\\src\\TXT\\input.txt");
+		BufferedReader br = new BufferedReader(reader);
+		String letters = "";
+		while(true){
+			String s = br.readLine();
+			if (s == null){
+				break;
+			}
+			letters +=getMatch(s);
+		}
+		System.out.println("属于中国'四级六位制'的邮政编码为   ："+letters);
+	}
+
+	private static String getMatch(String s) {
+		String regex = "[0-9]{6}";
+		String matches = "";
+		matches += _findMatch(regex, s);
+		return matches;
+	}
+
+	private static String _findMatch(String regex, String s) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher m = pattern.matcher(s);
+		String matches = "";
+		while(m.find()){
+			matches +="["+m.group(0)+"]；";
+		}
+		return matches;
+	}
+
+}
