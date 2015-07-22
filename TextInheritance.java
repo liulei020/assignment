eb23d87e5aeacd22de1f6eabd2958093fb8088ea
liulei020 <158675345@qq.com>
2015-07-22 09:08:47 +0800

    add assignment_7.1.4 about Text of Subclass Square's inheritance

---
+++ TextInheritance.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+import Inheritance.Square;
+
+public class TextInheritance {
+
+
+	public static void main(String[] args) {
+		Square s = new Square(20, "black");
+		s.printInfo();
+	}
+
+}
