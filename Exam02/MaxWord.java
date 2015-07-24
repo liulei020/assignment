4c62cd7a8d5e475d4ca3894ca0d887cd30465068
liulei020 <158675345@qq.com>
2015-07-24 12:00:16 +0800

    add Exam02 about the MaxWeigth of word

---
+++ Exam02/MaxWord.java
@@ -0,0 +1,27 @@
+package Exam02;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MaxWord {
+	public int getId(String[] ch){
+		int id=0;
+		return id;
+
+	}
+	public void setWeigth(int id){
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		FileReader reader = new FileReader("https://github.com/zhutao2015/" +
+				"intern-test/blob/master/test2/src.txt");
+		BufferedReader br = new BufferedReader(reader);
+		
+		String[] ch= {"am","is","are","was","been","has","have",
+				"had","a","an","the","in","at","on","to","or"};
+	}
+
+}
